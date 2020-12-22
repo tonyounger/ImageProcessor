f0b64a82455307144e2c0317b023f473e3685adb
tonyounger <tondav@qq.com>
2020-12-22 17:46:10 +0800

    First commit

---
+++ WindowsFormsApp1/WindowsFormsApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+
+            var workingDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            var ileName = Path.Combine(workingDirectory, "Win7X64");//, "CF-926AC.exe"
+        }
+    }
+}
